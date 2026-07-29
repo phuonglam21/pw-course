aa2415be284ea50fcc121d06e6847fccc56eb543
Lam Nguyen <phuonglap21112000@gmail.com>
2026-07-30 00:24:36 +0700

    aaa

---
+++ tests/lesson-01/02-loop.js
@@ -0,0 +1,18 @@
+// //Ứng dụng tìm số chẳn/lẻ
+// nếu là số lẻ,chia dư cho 2 = 1: x % 2 ===1 
+// nếu là số chẳn, chia hết cho 2 = 0: x % 2 === 0
+
+
+// const numBer = 10;
+// if ( numBer % 2) {
+//     console.log ("so chan")
+// }else ( numBer %3)
+//     console.log ("so le")
+
+const x =  10;
+if (     x % 2 && x % 3) {
+    Console.log("so chan")
+}else{
+    Console.log ("so le")
+}
+
