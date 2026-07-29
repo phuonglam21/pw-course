f407765d8eed371723701d24c7687a74aabafd5c
Lam Nguyen <phuonglap21112000@gmail.com>
2026-07-29 23:48:24 +0700

    lambaidine

---
+++ tests/lesson-01/01-toantu.js
@@ -0,0 +1,23 @@
+// //3%3 =0 (vi 3 chia het cho 3 du 0)
+// let numBer = 3;
+// const text1 = "Du";
+// console.log ( numBer %3 + "dư" );
+// //3%2 =1 (vi 3 chia ko het 2 du 1)
+// // console.log ( numBer %2 + "dư" );
+// console.log ( numBer%1 + "dư");
+// console.log(numBer + "/2 dư " + (number % 2));
+ 
+let number = 3;
+
+// Chỉ in số dư
+console.log(number % 2);
+// 1
+
+// Ghép số dư với chữ
+console.log(number % 2 + " dư");
+// 1 dư
+
+// Muốn in đầy đủ
+console.log(number + "/2 dư " + (number % 2));
+// 3/2 dư 1
+ 
