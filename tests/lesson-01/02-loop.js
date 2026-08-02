8e85b564574b6b9ae198adfbca43e13915a62609
Lam Nguyen <phuonglap21112000@gmail.com>
2026-08-02 16:18:18 +0700

    mmm,

--- tests/lesson-01/02-loop.js
+++ tests/lesson-01/02-loop.js
@@ -3,16 +3,17 @@
 // nếu là số chẳn, chia hết cho 2 = 0: x % 2 === 0
 
 
-// const numBer = 10;
-// if ( numBer % 2) {
-//     console.log ("so chan")
-// }else ( numBer %3)
-//     console.log ("so le")
+const numBer = 10;
+if ( numBer % 2 === 0 ) {
+    console.log ("so chan");
+}else ( numBer % 2 === 1 ) 
+     console.log ("so le");
 
-const x =  10;
-if (     x % 2 && x % 3) {
-    Console.log("so chan")
-}else{
-    Console.log ("so le")
-}
+
+// const x =  10;
+// if ( x % 2 && x % 3) {
+//     Console.log("so chan")
+// }else{
+//     Console.log ("so le")
+// }
 
